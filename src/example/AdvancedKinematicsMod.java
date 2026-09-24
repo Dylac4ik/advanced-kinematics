6733ffa13f3e578765057bf467886ff9ea63a75c
Dylanchik <wtegedbfg@gmail.com>
2026-09-24 12:16:03 +0300

    создана основа проекта

---
+++ src/example/AdvancedKinematicsMod.java
@@ -0,0 +1,20 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class ExampleJavaMod extends Mod{
+
+    public ExampleJavaMod(){
+        
+    }
+
+    @Override
+    public void loadContent(){
+
+    }
+
+}
