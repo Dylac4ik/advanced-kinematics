0d5d870a1c189f5fd7482f667d5b895238eeff8b
Dylanchik <wtegedbfg@gmail.com>
2026-09-24 12:36:33 +0300

    тест

--- src/example/AdvancedKinematicsMod.java
+++ src/example/AdvancedKinematicsMod.java
@@ -1,14 +1,13 @@
 package example;
-
 import arc.*;
 import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class AdvancedKinematicsMod extends Mod{
 
-    public ExampleJavaMod(){
+    public AdvancedKinematicsMod(){
         
     }
 
